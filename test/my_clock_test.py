0c9292d0c613d62243c7f0ced4c5f9ceaeab2438
yassu <mathyassu@gmail.com>
2016-11-03 16:25:51 +0900

    flake8 in test

--- test/my_clock_test.py
+++ test/my_clock_test.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+from my_clock import my_clock as pm
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-from my_clock import my_clock as pm
+
 
 def get_time_test1():
-    assert(pm.get_time(['1s', '2m', '3h']) == 3*60*60 + 2*60 + 1)
+    assert(pm.get_time(['1s', '2m', '3h']) == 3 * 60 * 60 + 2 * 60 + 1)
+
 
 def get_time_test2():
-    assert(pm.get_time(['100s', '2m', '3h']) == 3*60*60 + 2*60 + 100)
+    assert(pm.get_time(['100s', '2m', '3h']) == 3 * 60 * 60 + 2 * 60 + 100)
+
 
 def get_time_test3():
-    assert(pm.get_time(['100s', '2', '3h']) == 3*60*60 + 2*60 + 100)
+    assert(pm.get_time(['100s', '2', '3h']) == 3 * 60 * 60 + 2 * 60 + 100)
