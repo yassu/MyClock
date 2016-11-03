eba2f644749aa07b4898e0a978d8db8b115841d6
yassu <mathyassu@gmail.com>
2016-11-03 20:22:12 +0900

    fix test

--- test/my_clock_test.py
+++ test/my_clock_test.py
@@ -6,12 +6,12 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 
 
 def get_time_test1():
-    assert(pm.get_time(['1s', '2m', '3h']) == 3 * 60 * 60 + 2 * 60 + 1)
+    assert(pm.get_time(['1s', '2m', '3h'], []) == 3 * 60 * 60 + 2 * 60 + 1)
 
 
 def get_time_test2():
-    assert(pm.get_time(['100s', '2m', '3h']) == 3 * 60 * 60 + 2 * 60 + 100)
+    assert(pm.get_time(['100s', '2m', '3h'], []) == 3 * 60 * 60 + 2 * 60 + 100)
 
 
 def get_time_test3():
-    assert(pm.get_time(['100s', '2', '3h']) == 3 * 60 * 60 + 2 * 60 + 100)
+    assert(pm.get_time(['100s', '2', '3h'], []) == 3 * 60 * 60 + 2 * 60 + 100)
