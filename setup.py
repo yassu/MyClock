78f3041c906d55b5b6f98fac963cf8594e4c5b89
yassu <mathyassu@gmail.com>
2016-11-14 00:50:47 +0900

    add lisence element in setup

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     author='Yassu',
     author_email='mathyassu@gmail.com',
     url='https://github.com/yassu/MyClock',
+    license = 'MIT',
     packages=['my_clock'],
     install_requires=['json5'],
     classifiers=classifiers,
