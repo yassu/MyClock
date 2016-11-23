5e3ee546c0ab58de1232d95b0769ddd2614b5bcc
yassu <mathyassu@gmail.com>
2016-11-23 17:11:11 +0900

    update file-check

--- my_clock/my_clock.py
+++ my_clock/my_clock.py
@@ -388,6 +388,10 @@ def main():
         sys.stderr.write(ex.args[0] + '\n')
         sys.exit()
     options = merge_options(input_options, options, hide_options)
+    if options['bgm_filename'] is not None:
+        options['bgm_filename'] = os.path.expanduser(options['bgm_filename'])
+    if options['ring_bell'] is not None:
+        options['bell_sound'] = os.path.expanduser(options['bell_sound'])
 
     if opts.show:
         print('Options:')
