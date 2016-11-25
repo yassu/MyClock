e9e45abfe22870c205a331a252c47cee0e8b19f9
yassu <mathyassu@gmail.com>
2016-11-25 22:47:27 +0900

    flake8 for deploy

--- script/deploy.py
+++ script/deploy.py
@@ -5,7 +5,7 @@ print('Run nosetests')
 check_output(['nosetests'])
 print('Run flake8')
 check_output(['flake8', 'setup.py',
-                'my_clock/my_clock.py', 'test/my_clock_test.py'])
+              'my_clock/my_clock.py', 'test/my_clock_test.py'])
 
 # git tag version
 version = check_output(['python', 'my_clock/my_clock.py', '--version'])
